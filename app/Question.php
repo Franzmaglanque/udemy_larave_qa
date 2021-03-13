e3561d7e536f997db6208210f63482aff88d9828
Franz Maglaque <maglanquefranz@gmail.com>
2021-03-13 19:47:43 +0800

    added show questions #84 done

--- app/Question.php
+++ app/Question.php
@@ -20,7 +20,10 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        return route("questions.show", $this->id);
+        // return route("questions.show", $this->id);
+
+        // pass slug instead of id
+        return route("questions.show", $this->slug);
     }
 
     public function getCreatedDateAttribute()
@@ -41,5 +44,9 @@ class Question extends Model
             return "answered";
         }
         return "unanswered";
+    }
+
+    public function getBodyHtmlAttribute(){
+        return \Parsedown::instance()->text($this->body);
     }
 }
